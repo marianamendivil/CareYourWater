97e753cfe499baed4bc1332bbf0a658476493609
marianamendivil <mariana.mendidu@gmail.com>
2019-08-22 01:27:07 -0500

    navigation between pages and nav bar ready

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,53 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+window.onload = init;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+function init(){
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    //Navigtion
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    document.getElementById("loginP").style.display = "block";
 
-        console.log('Received Event: ' + id);
+    var signButton = document.getElementById("signButton");
+    signButton.addEventListener('click', navigate("loginP","formP"));
+
+    var formButton = document.getElementById("formButton");
+    formButton.addEventListener('click', ()=>{
+        navigate("formP","mainP")();
+        document.getElementById("navBar").style.display = "block";   
+    });
+
+    var hidriButton = document.getElementById("hidriButton");
+    hidriButton.addEventListener('click', hiding("hidriP"));
+
+    var mainButton = document.getElementById("mainButton");
+    mainButton.addEventListener('click', hiding("mainP"));
+
+    var profileButton = document.getElementById("profileButton");
+    profileButton.addEventListener('click', hiding("profileP"));
+}
+
+//This function shows the page that you select in the nav bar, and hides the other pages
+
+function hiding (id){
+    return function(){
+        var elements = document.getElementsByClassName("hiding");
+        [].forEach.call(elements, function (el) {
+            el.style.display = "none";
+        });
+        document.getElementById(id).style.display = "block";
     }
-};
+}
 
-app.initialize();
+function show (id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function hide (id) {
+    document.getElementById(id).style.display = "none";
+}
+
+var navigate = function(actual, next){
+    return function(){
+        hide(actual);
+        show(next);
+    }
+}
